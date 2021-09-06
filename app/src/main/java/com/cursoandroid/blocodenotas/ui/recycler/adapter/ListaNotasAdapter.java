7c6e012f4298fef7d9fb334fefe70764b327c3f8
Sandro Araujo Barbosa <sanbarbosaa@gmail.com>
2021-09-06 20:18:02 -0300

    first commit

---
+++ app/src/main/java/com/cursoandroid/blocodenotas/ui/recycler/adapter/ListaNotasAdapter.java
@@ -0,0 +1,104 @@
+package com.cursoandroid.blocodenotas.ui.recycler.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cursoandroid.blocodenotas.R;
+import com.cursoandroid.blocodenotas.model.Nota;
+import com.cursoandroid.blocodenotas.ui.recycler.adapter.listener.OnItemClickListener;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ListaNotasAdapter extends RecyclerView.Adapter<ListaNotasAdapter.NotaViewHolder> {
+
+    private final List<Nota> notas;
+    private final Context context;
+    private OnItemClickListener onItemClickListener;
+
+    public ListaNotasAdapter(Context context, List<Nota> notas) {
+        this.context = context;
+        this.notas = notas;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    @Override
+    public ListaNotasAdapter.NotaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false);
+
+        return new NotaViewHolder(viewCriada);
+    }
+
+    @Override
+    public void onBindViewHolder(ListaNotasAdapter.NotaViewHolder holder, int position) {
+        Nota nota = notas.get(position);
+        holder.vincula(nota);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notas.size();
+    }
+
+    public void altera(int posicao, Nota nota) {
+        notas.set(posicao, nota);
+        notifyItemChanged(posicao);
+    }
+
+    public void remove(int posicao) {
+        notas.remove(posicao);
+        notifyItemRemoved(posicao);
+    }
+
+    public void troca(int posicaoInicial, int posicaoFinal) {
+        Collections.swap(notas, posicaoInicial, posicaoFinal);
+        notifyItemMoved(posicaoInicial, posicaoFinal);
+    }
+
+    class NotaViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView titulo;
+        private final TextView descricao;
+        private Nota nota;
+
+        public NotaViewHolder(View itemView) {
+            super(itemView);
+            titulo = itemView.findViewById(R.id.item_nota_titulo);
+            descricao = itemView.findViewById(R.id.item_nota_descricao);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   onItemClickListener.onItemClick(nota, getAdapterPosition() );
+                }
+            });
+        }
+
+        public void vincula(Nota nota) {
+            this.nota = nota;
+            preencheCampos(nota);
+        }
+
+        private void preencheCampos(Nota nota) {
+            titulo.setText(nota.getTitulo());
+            descricao.setText(nota.getDescricao());
+        }
+    }
+
+    public void adiciona(Nota nota) {
+        notas.add(nota);
+        notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/cursoandroid/blocodenotas/ui/recycler/adapter/listener/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package com.cursoandroid.blocodenotas.ui.recycler.adapter.listener;
+
+import com.cursoandroid.blocodenotas.model.Nota;
+
+public interface OnItemClickListener {
+
+    void onItemClick(Nota nota, int posicao);
+}
